09e755b4abd37374ba43114136d80287dba24dd7
Baskov-Aleksey-Viktorovich <777baskovaleksey777@gmail.com>
2024-07-22 11:51:23 +0300

    fix bug

--- php/get_key.php
+++ php/get_key.php
@@ -4,6 +4,11 @@ include_once 'db.php';
 $database = new Database();
 $obj = $database->get_token();
 
-header('Content-Type: application/json');
-echo json_encode($obj);
+if (isset($obj->access_token)) {
+    header('Content-Type: application/json');
+    echo json_encode($obj);
+} else {
+    header('Content-Type: application/json');
+    echo json_encode(['error' => 'Не вдалося отримати ключ.']);
+}
 ?>
